2f426a0fcd6752e1695c48fcedfd953d782b5ff8
Kelyan3 <62492542+Kelyan3@users.noreply.github.com>
2026-07-29 14:00:23 +1100

    feat(SCSS): Modification des couleurs pour mode sombre/éclairé.

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,7 +4,7 @@ const ThemeContext = createContext(null);
 
 export const ThemeProvider = ({ children }) => {
     const [theme, setTheme] = useState(() => {
-        return localStorage.getItem("theme") || "dark";
+        return localStorage.getItem("theme") || "light";
     });
 
     useEffect(() => {
